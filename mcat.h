60fe052ecc4ce77836ebd6651b753a57eded6b2f
Matvey Soloviev <blackhole89@gmail.com>
2023-07-11 04:23:58 +0200

    Rework in light of not having to use sharps

--- mcat.h
+++ mcat.h
@@ -3,187 +3,221 @@
 #include<unordered_map>
 #include<functional>
 #include<set>
+#include<armadillo>
 
 /* abstract interface to markov categories */
-template<class M> struct Obj {
-    virtual M* id()=0;
-    virtual M* samp()=0;
+struct Mor;
+struct Obj {
+    virtual Mor* id()=0;
+    virtual int tensor_length()=0;
+    virtual Mor* copy_n(int n)=0;
+    virtual bool is_equal(Obj *other)=0;
+    virtual Obj* tensor(Obj *other)=0;
+    virtual Mor* term()=0; // morphism to terminal object
+
+    virtual Mor* select(int n)=0; // morphism to nth tensee, technically derivable but we chose to not expose the tensor structure at this stage...
+
+    virtual void debug_print()=0;
+
+    // derived from the others
+    Mor* permute_project(std::vector<int> v);
 };
-template<class M, class O> struct Mor {
-    virtual O* dom()=0;
-    virtual O* cod()=0;
-    virtual M* sharp()=0;
-    virtual M* cond(std::vector<O*> os)=0;
+struct Mor {
+    virtual Obj* dom()=0;
+    virtual Obj* cod()=0;
+    virtual Mor* cond(std::vector<Obj*> os)=0;
+    virtual Mor* compose(Mor* other)=0;
+    virtual Mor* tensor(Mor *other)=0;
+
+    virtual void debug_print()=0;
 };
 
-template<class M, class O> struct MCat {
-    virtual M* arrange(std::vector<int>, std::vector<M*>)=0;
-    virtual M* compose(M*, M*)=0;
-};
 
+Mor *Obj::permute_project(std::vector<int> v)
+{
+    Mor *delta = copy_n(v.size());
+    Mor *join = NULL;
+    for(int i=0;i<v.size();++i) {
+        Mor *selectone = select(v[i]);
+        
+        if(join) join = join->tensor(selectone);
+        else join = selectone;
+    }
+    return delta->compose(join);
+}
 
 
 /* an enumerable domain for finstoch */
-template<class P, int n> struct FinStochVal {
-    int order;
-    const int max = n;
+using namespace arma;
 
-    std::unordered_map<FinStochVal<P,n>*, P> tab;
-};
+struct FinStochMor;
+struct FinStochObj : public Obj {
+    // ASSUME there are no zeroes in here
+    std::vector<unsigned int> sizes;
 
-template<class P, class V> struct FinStochMor;
-
-template<class P, class V> struct FinStochObj : public Obj<FinStochMor<P,V> > {
-    using Mor = FinStochMor<P,V>;
-    using Obj = FinStochObj<P,V>;
-    virtual Mor *id();
-    virtual Mor *samp();
-
-    std::vector<int> type; 
-};
-
-template<class P, class V> struct FinStochMor : public Mor<FinStochMor<P,V>, FinStochObj<P,V> > {
-    using Mor = FinStochMor<P,V>;
-    using Obj = FinStochObj<P,V>;
-    virtual Obj* dom() { return src; };
-    virtual Obj* cod() { return tgt; };
-    virtual Mor* sharp();
-    virtual Mor* cond(std::set<int> os);
-
-    Obj *src,*tgt;
-    virtual std::vector<V*> eval_on(std::vector<V*> v)=0;
-};
-
-template<class P, class V> struct FinStoch : public MCat<FinStochMor<P,V>, FinStochObj<P,V> > {
-    using Mor = FinStochMor<P,V>;
-    using Obj = FinStochObj<P,V>;
-    virtual Mor* arrange(std::vector<int>, std::vector<Mor*>);
-    virtual Mor* compose(Mor*, Mor*);
-};
-
-template<class P, class V> FinStochMor<P,V> *FinStoch<P,V>::arrange(std::vector<int>, std::vector<FinStochMor<P,V>*>){
-    return NULL;
-}
-
-/* composition of morphisms */
-template<class P, class V> struct FinStochMorComposition : public FinStochMor<P,V> {
-    using Mor = FinStochMor<P,V>;
-    Mor *m1, *m2;
-    virtual std::vector<V*> eval_on(std::vector<V*> v) {
-        m2->eval_on(m1->eval_on(v));
+    virtual Mor* id();
+    virtual int tensor_length() {
+        return sizes.size();
     }
-};
+    virtual Mor* copy_n(int n);
 
-template<class P, class V> FinStochMor<P,V> *FinStoch<P,V>::compose(FinStochMor<P,V>* m1, FinStochMor<P,V>* m2) {
-    if (m1->cod()->type != m2->dom()->type) return NULL;
+    virtual bool is_equal(Obj *other) {
+        // assume conversion goes through
+        FinStochObj *fsother = (FinStochObj*)other;
 
-    FinStochMorComposition<P,V> *ret = new FinStochMorComposition<P,V>;
-    ret->src=m1->src;
-    ret->tgt=m2->tgt;
-    ret->m1=m1;
-    ret->m2=m2;
-    return ret;
-}
-
-
-/* id and samp morphisms on an object */
-template<class P, class V> struct FinStochMorId : public FinStochMor<P,V> {
-    virtual std::vector<V*> eval_on(std::vector<V*> v) {
-        return v;
+        return (sizes == fsother->sizes);
     }
-};
+    virtual FinStochObj* tensor(Obj *other) {
+        // assume conversion goes through
+        FinStochObj *fsother = (FinStochObj*)other;
 
-template<class P, class V> FinStochMor<P,V> *FinStochObj<P,V>::id() {
-    FinStochMorId<P,V> *ret = new FinStochMorId<P,V>;
-    ret->src=ret->tgt=this;
-    return ret;
-}
+        FinStochObj *ret = new FinStochObj();
 
-template<class P, class V> struct FinStochMorSamp : public FinStochMor<P,V> {
-    virtual std::vector<V*> eval_on(std::vector<V*> v) {
-        // ASSUME v.size()==1
-        V *vr = new V;
-        vr->order = v[0]->order-1;
+        ret->sizes.insert( ret->sizes.end(), sizes.begin(), sizes.end() );
+        ret->sizes.insert( ret->sizes.end(), fsother->sizes.begin(), fsother->sizes.end() );
 
-        // this probably doesn't work for non-singletons, need true equality check
-        for(auto &[k,v] : v[0]->tab) {
-            for(auto &[k1,v1] : k->tab) {
-                vr->tab[k1] += v*v1;
-            }
+        return ret;
+    }
+    virtual Mor* term();
+    virtual Mor* select(int n);
+
+    FinStochObj() {}
+    FinStochObj(unsigned int _size) { if(_size) sizes.push_back(_size); }
+
+    unsigned int total_dim() {
+        unsigned int r=1;
+        for(int i=0;i<sizes.size();++i) r*=sizes[i];
+        return r;
+    }
+
+    virtual void debug_print() {
+        printf("FinStochObj %X: ", this);
+        if(!sizes.size()) printf("I\n");
+        else {
+            printf("[%d]",sizes[0]);
+            for(int i=1;i<sizes.size();++i) printf("x[%d]",sizes[i]);
+            printf("\n");
         }
     }
+
+    int tuple2coord(std::vector<int> v) {
+        if(v.size() != sizes.size()) return -1;
+
+        int stride=1;
+        int ret=0;
+        for(int i=v.size()-1;i>=0;--i) {
+            ret += stride * v[i];
+            stride *= sizes[i];
+        }
+        return ret;
+    }
+    // deterministic morphism to tuple
+    FinStochMor *det_const(std::vector<int>);
 };
 
-template<class P, class V> FinStochMor<P,V> *FinStochObj<P,V>::samp() {
-    if(type.size()!=1) return NULL; // only know how to sample single RVs
-    if(type[0]<1) return NULL;
+struct FinStochMor : public Mor {
+    FinStochObj *o_dom, *o_cod;
+    mat data;
 
-    FinStochMorSamp<P,V> *ret = new FinStochMorSamp<P,V>;
-    ret->src=this;
-    ret->tgt=new FinStochObj<P,V>;
-    ret->tgt->type=type;
-    ret->tgt->type[0]--;
+    virtual FinStochObj* dom() { return o_dom; }
+    virtual FinStochObj* cod() { return o_cod; }
+    virtual FinStochMor* cond(std::vector<Obj*> os) {};
+    virtual FinStochMor* compose(Mor* other) {
+        if(!other->dom()->is_equal(cod())) return NULL;
 
+        // assume conversion goes through
+        FinStochMor *fsother = (FinStochMor*)other;
+
+        FinStochMor *ret = new FinStochMor();
+        ret->o_dom = o_dom;
+        ret->o_cod = fsother->o_cod;
+        ret->data = data * fsother->data;
+        return ret;
+    }
+    virtual FinStochMor* tensor(Mor *other) {
+        FinStochMor *ret = new FinStochMor();
+
+        // assume conversion goes through
+        FinStochMor *fsother = (FinStochMor*)other;
+
+        ret->o_dom = o_dom->tensor(fsother->o_dom);
+        ret->o_cod = o_cod->tensor(fsother->o_cod);
+        ret->data = kron(data, fsother->data);
+
+        return ret;
+    }
+
+    virtual void debug_print() {
+        printf("== FinStochMor %X ==\n", this);
+        printf("dom: ");
+        o_dom->debug_print();
+        printf("cod: ");
+        o_cod->debug_print();
+        data.print(std::cout);
+    }
+};
+
+Mor* FinStochObj::id()
+{
+    FinStochMor *ret = new FinStochMor();
+    ret->o_dom = ret->o_cod = this;
+    ret->data = eye(total_dim(), total_dim());
     return ret;
 }
 
-/* sharpen */
-template<class P, class V> struct FinStochMorSharp : public FinStochMor<P,V> {
-    using Mor = FinStochMor<P,V>;
-    Mor *minner;
-    virtual std::vector<V*> eval_on(std::vector<V*> v) {
-        std::vector<V*> d = minner->eval_on(v);
-        V *vr = new V;
-        vr->order = v[0]->order+1;
-
-        vr->tab[v[0]] = 1.0;
-
-        return std::vector<V*> { vr };
-    }
-};
-
-template<class P, class V> FinStochMor<P,V> *FinStochMor<P,V>::sharp() {
-    if(tgt->type.size()!=1) return NULL; // only know how to sample single RVs
-
-    FinStochMorSharp<P,V> *ret = new FinStochMorSharp<P,V>;
-    ret->src = src;
-    ret->tgt=new FinStochObj<P,V>;
-    ret->tgt->type = tgt->type;
-    ret->tgt->type[0]++;
-    ret->minner = this;
-    return ret; 
+Mor* FinStochObj::term()
+{
+    FinStochMor *ret = new FinStochMor();
+    ret->o_dom = this;
+    ret->o_cod = new FinStochObj();
+    ret->data = ones(total_dim(), 1);
+    return ret;
 }
 
-/* condition */
-template<class P, class V> struct FinStochMorCond : public FinStochMor<P,V> {
-    using Mor = FinStochMor<P,V>;
-    Mor *minner;
-    std::set<int> os;
-    virtual std::vector<V*> eval_on(std::vector<V*> v) {
-        V *vr = new V;
-
-        return std::vector<V*> { vr };
+Mor* FinStochObj::copy_n(int n)
+{
+    FinStochMor *ret = new FinStochMor();
+    ret->o_dom = this;
+    ret->o_cod = this;
+    int vs = total_dim();
+    for(int i=1;i<n;++i) {
+        ret->o_cod = ret->o_cod->tensor(this);
+        vs *= total_dim();
     }
-};
+    ret->data = zeros( total_dim(), vs );
 
-template<class P, class V> FinStochMor<P,V> *FinStochMor<P,V>::cond(std::set<int> os) {
-    FinStochMorCond<P,V> *ret = new FinStochMorCond<P,V>;
-    ret->src=new FinStochObj<P,V>;
-    ret->src->type = src->type;
-    ret->tgt=new FinStochObj<P,V>;
-    ret->tgt->type = tgt->type;
-    // meanings of indices shift as we erase
-    int shift=0;
-    for(int i : os) {
-        // make this output an input
-        ret->src->type.push_back(ret->tgt->type[i-shift]);
-        // erase it from the list of outputs
-        ret->tgt->type.erase(ret->tgt->type.begin()+(i-shift));
-        ++shift;
+    int geosum = (vs-1)/(total_dim()-1);
+    vs /= total_dim();
+    for(int i=0;i<total_dim();++i) {
+        ret->data(i,i*geosum)=1;
     }
-    ret->minner = this;
-    return ret; 
+    return ret;
 }
 
+Mor* FinStochObj::select(int n)
+{
+    // optimised, but could be constructed from term and id
+    FinStochMor *ret = new FinStochMor();
+    ret->o_dom = this;
+    ret->o_cod = new FinStochObj(sizes[n]);
 
+    unsigned int diml=1,dimr=1;
+    for(int i=0;i<n;++i) {
+        diml *= sizes[i];
+    }
+    for(int i=(n+1);i<sizes.size();++i) {
+        dimr *= sizes[i];
+    }
+    ret->data = kron(kron(ones(diml,1),eye(sizes[n],sizes[n])),ones(dimr,1));
+    return ret;
+}
 
+FinStochMor *FinStochObj::det_const(std::vector<int> v)
+{
+    FinStochMor *ret = new FinStochMor();
+    ret->o_dom = new FinStochObj();
+    ret->o_cod = this;
+    ret->data = zeros( 1, total_dim() );
+    ret->data( 0, tuple2coord(v) ) = 1;
+    return ret;
+}
--- test.cpp
+++ test.cpp
@@ -2,6 +2,20 @@
 
 int main(int argc, char* argv[])
 {
-    FinStoch<float, FinStochVal<float, 2> > fs;
+//    FinStoch<float, FinStochVal<float, 2> > fs;
+
+    FinStochObj *two = new FinStochObj(2);
+    FinStochObj *three = new FinStochObj(3);
+    FinStochObj *tt = two->tensor(three);
+    tt->select(1)->debug_print();
+    two->copy_n(3)->debug_print();
+    tt->permute_project({1,0})->debug_print();
+    tt->permute_project({1})->debug_print();
+
+    tt->det_const({0,2})->compose(tt->permute_project({1}))->debug_print();
+
+    FinStochObj *ttt = tt->tensor(three);
+    ttt->det_const({0,1,2})->compose(ttt->permute_project({0,2,1}))->compose(ttt->permute_project({1}))->debug_print();
+
     return 0;
 }
